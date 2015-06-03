75bcba3487ebb1efa3b30b9011a4bf8fe4c3cc2c
michepasto <michepasto@gmail.com>
2015-06-03 10:34:03 +0200

    startup

---
+++ devApp/www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+  var deviceInfo = cordova.require("cordova/plugin/DeviceInformation");
+  deviceInfo.get(function(result) {
+    console.log("result = " + result);
+  }, function() {
+    console.log("error");
+  });
+});
